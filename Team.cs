e7f6e11fcab25cce2759205a7112fa2714fbc6ee
AYZBTR <101973630+AYZBTR@users.noreply.github.com>
2022-04-30 20:51:37 +0300

    Add files via upload

---
+++ Team.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERIALIZATION_AND_DESERIALIZATION
+{
+    public class Team
+    {
+        public Team(string name, DateTime dateOfBirth, List<Continfo> contactInformation)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            ContactInformation = contactInformation;
+        }
+
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public List<Continfo> ContactInformation { get; set; }
+        public string Email { get; internal set; }
+        public string Mobile { get; internal set; }
+    }
+
+    public class Continfo
+    {
+        public Continfo(string email, string mobile)
+        {
+            Email = email;
+            Mobile = mobile;
+        }
+
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+    }
+
+
+}
+
+
+
+
+   
+
